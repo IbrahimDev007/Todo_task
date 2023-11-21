1e7433d386f96faca1818083d90be8b277980c52
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-22 01:08:56 +0600

    table update

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -1,9 +1,9 @@
 const Card = () => {
 	return (
-		<div className="grid  pt-10 gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 bg-violet-700 shadow-inner-lg ">
-			<div className="card w-96  shadow-xl bg-blue-600">
-				<button className="btn btn-sm glass">Todo</button>
-				<form className="card-body">
+		<div className="grid  pt-10 gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 bg-sky-400 shadow-inner-lg ">
+			<div className="card w-96  shadow-xl bg-blue-600  min-h-screen ">
+				<button className="btn btn-sm glass text-white">Todo</button>
+				<form className="card-body mb-4">
 					<div className="form-control">
 						<label className="label">
 							<span className="label-text text-whit font-bold">Title</span>
@@ -31,38 +31,82 @@ const Card = () => {
 						</button>
 					</div>
 				</form>
+
+				<div className="overflow-x-auto">
+					<table className="table">
+						{/* head */}
+						<thead>
+							<tr>
+								<th>
+									<label>
+										<input type="checkbox" className="checkbox" />
+									</label>
+								</th>
+								<th>Name</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* row 1 */}
+							<tr>
+								<th>
+									<label>
+										<input type="checkbox" className="checkbox" />
+									</label>
+								</th>
+								<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
+									<h3 className="text-xl font-bold">Title</h3>
+									<p className="font-semibold">Description</p>
+									<h2 className="text-sm font-mono">status</h2>
+								</div>
+							</tr>
+						</tbody>
+						{/* foot */}
+					</table>
+				</div>
+			</div>
+			<div className="card w-96 shadow-xl bg-sky-500">
+				<button className="btn btn-sm glass text-primary">Pogress</button>
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
+					<h3 className="text-xl font-bold">Title</h3>
+					<p className="font-semibold">Description</p>
+					<h2 className="text-sm font-mono">status</h2>
+				</div>
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[7rem]">
+					<h3 className="text-xl font-bold">Title</h3>
+					<p className="font-semibold">Description</p>
+					<h2 className="text-sm font-mono">status</h2>
+				</div>
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
+					<h3 className="text-xl font-bold">Title</h3>
+					<p className="font-semibold">Description</p>
+					<h2 className="text-sm font-mono">status</h2>
+				</div>
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
+					<h3 className="text-xl font-bold">Title</h3>
+					<p className="font-semibold">Description</p>
+					<h2 className="text-sm font-mono">status</h2>
+				</div>
+			</div>
+			<div className="card w-96  bg-sky-700 shadow-xl">
+				<button className="btn btn-sm glass text-info">Done</button>
 				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
 					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-">description</p>
+					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>
 				</div>
-				<div className="card-body border-4 shadow-lg solid border-blue-700 bg-sky-700 rounded-md">
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
 					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-">description</p>
+					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>
 				</div>
-				<div className="card-body border-4 shadow-lg solid border-blue-700 bg-sky-700 rounded-md">
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
 					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-">description</p>
+					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>
 				</div>
-				<div className="card-body border-4 shadow-lg solid border-blue-700 bg-sky-700 rounded-md">
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
 					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-">description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-			</div>
-			<div className="card w-96 bg-base-100 shadow-xl">
-				<div className="card-body border-2 solid border-blue-700">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semiBold">description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-			</div>
-			<div className="card w-96 bg-base-100 shadow-xl">
-				<div className="card-body border-2 solid border-blue-700">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semiBold">description</p>
+					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>
 				</div>
 			</div>

123e20d9bf7c5ac5f1414cec297046b4feeae7c8
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-22 11:11:32 +0600

    modified card

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -1,6 +1,6 @@
 const Card = () => {
 	return (
-		<div className="grid  pt-10 gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 bg-sky-400 shadow-inner-lg ">
+		<div className="grid  pt-10 gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 text-white bg-sky-400 shadow-inner-lg ">
 			<div className="card w-96  shadow-xl bg-blue-600  min-h-screen ">
 				<button className="btn btn-sm glass text-white">Todo</button>
 				<form className="card-body mb-4">
@@ -31,57 +31,25 @@ const Card = () => {
 						</button>
 					</div>
 				</form>
-
-				<div className="overflow-x-auto">
-					<table className="table">
-						{/* head */}
-						<thead>
-							<tr>
-								<th>
-									<label>
-										<input type="checkbox" className="checkbox" />
-									</label>
-								</th>
-								<th>Name</th>
-							</tr>
-						</thead>
-						<tbody>
-							{/* row 1 */}
-							<tr>
-								<th>
-									<label>
-										<input type="checkbox" className="checkbox" />
-									</label>
-								</th>
-								<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
-									<h3 className="text-xl font-bold">Title</h3>
-									<p className="font-semibold">Description</p>
-									<h2 className="text-sm font-mono">status</h2>
-								</div>
-							</tr>
-						</tbody>
-						{/* foot */}
-					</table>
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 relative border-blue-700  bg-sky-700 rounded-md  max-h-40 m-4">
+					<input
+						type="checkbox"
+						checked="checked"
+						className="checkbox  absolute z-10 top-0 right-0"
+					/>
+					<h3 className="text-xl font-bold">Title</h3>
+					<p className="font-semibold">Description</p>
+					<h2 className="text-sm font-mono">status</h2>
 				</div>
 			</div>
 			<div className="card w-96 shadow-xl bg-sky-500">
 				<button className="btn btn-sm glass text-primary">Pogress</button>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[7rem]">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md min-h-[1.3rem]">
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 relative border-blue-700  bg-sky-700 rounded-md  max-h-40 m-4">
+					<input
+						type="checkbox"
+						checked="checked"
+						className="checkbox  absolute z-10 top-0 right-0"
+					/>
 					<h3 className="text-xl font-bold">Title</h3>
 					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>
@@ -89,22 +57,12 @@ const Card = () => {
 			</div>
 			<div className="card w-96  bg-sky-700 shadow-xl">
 				<button className="btn btn-sm glass text-info">Done</button>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
-					<h3 className="text-xl font-bold">Title</h3>
-					<p className="font-semibold">Description</p>
-					<h2 className="text-sm font-mono">status</h2>
-				</div>
-				<div className="card-body border-4 shadow-lg solid p-4 mb-4 border-blue-700  bg-sky-700 rounded-md">
+				<div className="card-body border-4 shadow-lg solid p-4 mb-4 relative border-blue-700  bg-sky-700 rounded-md max-h-40 m-4">
+					<input
+						type="checkbox"
+						checked="checked"
+						className="checkbox  absolute z-10 top-0 right-0"
+					/>
 					<h3 className="text-xl font-bold">Title</h3>
 					<p className="font-semibold">Description</p>
 					<h2 className="text-sm font-mono">status</h2>

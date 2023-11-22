6940892427eda9a26e0b21c20b43512edbb91f24
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-22 11:57:06 +0600

    react hook form and icon use

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import Hero from "./Component/Hero";
 import Card from "./Component/card";
+import { FcTodoList } from "react-icons/fc";
+import { ImSpinner9 } from "react-icons/im";
 
 const App = () => {
 	return (
 		<div>
 			<Hero />
-			<ul className="menu bg-base-200 lg:menu-horizontal rounded-box ">
-				<li>
-					<a>
-						Move to Todo
-						<span className="badge badge-sm">99+</span>
-					</a>
-				</li>
-				<li>
-					<a>
-						Move to pogress
-						<span className="badge badge-sm badge-warning">NEW</span>
-					</a>
-				</li>
-				<li>
-					<a>
-						Move To done
-						<span className="badge badge-xs badge-info"></span>
-					</a>
-				</li>
-			</ul>
+			<div className="flex justify-center items-center">
+				<ul className="menu bg-base-200 lg:menu-horizontal rounded-box ">
+					<li>
+						<h1>
+							<FcTodoList className="mx-1" />
+							Move to Todo
+							<span className="badge badge-sm">99+</span>
+						</h1>
+					</li>
+					<li>
+						<h1>
+							Move to pogress
+							<span className="badge badge-sm badge-warning">
+								<ImSpinner9 className="mx-1" />
+							</span>
+						</h1>
+					</li>
+					<li>
+						<h1>
+							Move To done
+							<span className="badge badge-xs badge-info"></span>
+						</h1>
+					</li>
+				</ul>
+			</div>
 			<Card />
 		</div>
 	);
--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -1,18 +1,27 @@
+import { useForm } from "react-hook-form";
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
+import { FaSpinner } from "react-icons/fa";
+import { SiTodoist } from "react-icons/si";
 const Card = () => {
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => console.log(data);
 	return (
 		<div className="grid  pt-10 gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-3 text-white bg-sky-400 shadow-inner-lg ">
 			<div className="card w-96  shadow-xl bg-blue-600  min-h-screen ">
-				<button className="btn btn-sm glass text-white">Todo</button>
-				<form className="card-body mb-4">
+				<button className="btn btn-sm glass text-white">
+					<SiTodoist className="text-xl  text-white" />
+					Todo
+				</button>
+				<form className="card-body mb-4" onSubmit={handleSubmit(onSubmit)}>
 					<div className="form-control">
 						<label className="label">
-							<span className="label-text text-whit font-bold">Title</span>
+							<span className="label-text text-white font-bold">Title</span>
 						</label>
 						<input
 							type="text"
 							placeholder="Title"
-							className="input input-bordered"
-							required
+							{...register("title", { required: true, maxLength: 10 })}
+							className="input input-bordered text-black"
 						/>
 					</div>
 					<div className="form-control">
@@ -20,13 +29,17 @@ const Card = () => {
 							<span className="label-text text-white">Description</span>
 						</label>
 						<textarea
-							className="textarea textarea-success"
+							className="textarea textarea-success  text-black"
 							placeholder="Description"
-							required
+							{...register("discription", { required: true, maxLength: 30 })}
 						></textarea>
 					</div>
 					<div className="form-control mt-6">
-						<button className="btn  btn-sm btn-success text-white font-bold">
+						<button
+							type="submit"
+							className="btn  btn-sm btn-success text-white font-bold"
+						>
+							<SiTodoist className="text-2xl text-white mx-2" />
 							Add Todo
 						</button>
 					</div>
@@ -43,7 +56,10 @@ const Card = () => {
 				</div>
 			</div>
 			<div className="card w-96 shadow-xl bg-sky-500">
-				<button className="btn btn-sm glass text-primary">Pogress</button>
+				<button className="btn btn-sm glass text-primary">
+					<FaSpinner className="text-2xl text-white" />
+					Pogress
+				</button>
 				<div className="card-body border-4 shadow-lg solid p-4 mb-4 relative border-blue-700  bg-sky-700 rounded-md  max-h-40 m-4">
 					<input
 						type="checkbox"
@@ -56,7 +72,10 @@ const Card = () => {
 				</div>
 			</div>
 			<div className="card w-96  bg-sky-700 shadow-xl">
-				<button className="btn btn-sm glass text-info">Done</button>
+				<button className="btn btn-sm glass text-info">
+					<IoCheckmarkDoneCircleOutline className="text-2xl text-white" />
+					Done
+				</button>
 				<div className="card-body border-4 shadow-lg solid p-4 mb-4 relative border-blue-700  bg-sky-700 rounded-md max-h-40 m-4">
 					<input
 						type="checkbox"
